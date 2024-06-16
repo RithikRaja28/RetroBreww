ab97dacbc820baa55d63e98f15825564dbddc52e
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-16 13:00:17 +0530

    firebase auth

---
+++ vintage_java/src/components/Authentication/Auth.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCx1Hv2bfz0dRgoYwqoFG0-scQ0pvi_gu8",
+  authDomain: "vintage-java.firebaseapp.com",
+  projectId: "vintage-java",
+  storageBucket: "vintage-java.appspot.com",
+  messagingSenderId: "202794637575",
+  appId: "1:202794637575:web:3d7f034e44e3f98f0b0da2",
+  measurementId: "G-V1Q5EL66JG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
