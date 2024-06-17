67ced474a66cf0a4c420887afea2162b50ab4d3a
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-17 22:03:40 +0530

    set up signup - bootstrap

--- vintage_java/src/App.jsx
+++ vintage_java/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import LandingPage from './components/LandingPAge/LandingPage'
+import Login from './components/UserAuth/Login'
+import Signup from './components/UserAuth/Signup'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/user-login" element={<Login />} />
+        <Route path="/user-signup" element={<Signup />} />
       </Routes>
     </div>
   )
