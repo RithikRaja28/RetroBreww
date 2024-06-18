8ff703cbab06531ac58816db8f4b9b0068fa0624
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-18 21:14:25 +0530

    navbar update

--- vintage_java/src/components/LandingPAge/LandingPage.jsx
+++ vintage_java/src/components/LandingPAge/LandingPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const LandingPage = () => {
   return (
-    <div>
-      
+    <div className="d-flex flex-column h-100">
+      <Navbar/>
+      <div className="container-fluid">
+        <div className="row flex-grow-1">
+          <div className="col-12 d-flex align-items-center justify-content-center">
+            <h1></h1>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default LandingPage
+
--- vintage_java/src/components/Navbar/Navbar.jsx
+++ vintage_java/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
-
+import React from "react";
+import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="m-1" href="/retrobrew">
+          <img
+            src="https://www.freepnglogos.com/uploads/coffee-logo-png/coffee-shop-clipart-images-34.png"
+            width="30"
+            height="30"
+            alt="Brand Logo"
+          />
+        </a>
+        <a
+          class="navbar-brand m-1 mt-2"
+          style={{
+            fontSize: "30px",
+            fontWeight: "bold",
+            textDecoration: "none",
+            fontStyle: "italic",
+            fontFamily: "serif",
+          }}
+          href="#"
+        >
+          <span style={{ color: "#A0522D" }}>R</span>etro<span style={{ color: "#A0522D" }}>B</span>rew
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-export default Navbar
+        <div
+          class="collapse navbar-collapse justify-content-center"
+          id="navbarTogglerDemo02"
+        >
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0 justify-content-center">
+            <li class="nav-item">
+              <Link class="nav-link active" to="/retrobrew">
+                Home
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link active" to="/">
+                Brew
+              </Link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
